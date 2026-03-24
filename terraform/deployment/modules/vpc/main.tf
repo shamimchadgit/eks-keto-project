c5bdbf9c4cb68e9f91542d933f1b2008a6e5f2bf
shamimchadgit <shamimchaudhury133@gmail.com>
2026-03-24 17:24:25 +0000

    Add modules directory for my deployment aws resources

---
+++ terraform/deployment/modules/vpc/main.tf
@@ -0,0 +1,101 @@
+data "aws_availability_zones" "availability" {
+    state = "available"
+}
+
+# VPC
+resource "aws_vpc" "main" {
+    cidr_block = var.cidr_block
+    enable_dns_support = true
+    enable_dns_hostnames = true
+
+    tags = {
+      Name = "eks-vpc"
+    }
+}
+# Public Subnet
+resource "aws_subnet" "public_subnet" {
+    count = length(var.public_subnets_cidrs)
+    vpc_id = aws_vpc.main.id
+    cidr_block = [var.public_subnets_cidrs]
+    availability_zone = data.aws_availability_zones.availability.names [count.index]
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "public-subnet-${count.index +1}"
+    }
+}
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+    count = length(var.private_subnets_cidrs)
+    vpc_id = aws_vpc.main.id
+    cidr_block = [var.private_subnets_cidrs]
+    availability_zone = data.aws_availability_zones.availability.names [count.index]
+    map_public_ip_on_launch = false
+
+    tags = {
+        Name = "private-subnet-${count.index +1}"
+    }
+}
+# IGW
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+      Name = "eks-igw"
+    }
+}
+# Route Table - Public
+resource "aws_route_table" "pb_rt" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+    tags = {
+      Name = "public-rt"
+    }
+}
+# Route Table Association - Public
+resource "aws_route_table_association" "pb_rt_as" {
+    count = length(aws_subnet.public_subnet)
+    subnet_id = aws_subnet.public_subnet[count.index].id
+    route_table_id = aws_route_table.pb_rt
+}
+# Route Table - Private
+resource "aws_route_table" "pr_rt" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat_gw.id
+    }
+    tags = {
+      Name = "private-rt"
+    }
+}
+# Route Table Association - Private
+resource "aws_route_table_association" "pr_rt_as" {
+    count = length(aws_subnet.private_subnet)
+    subnet_id = aws_subnet.private_subnet[count.index].id
+    route_table_id = aws_route_table.pr_rt
+}
+
+# Elastic IP
+resource "aws_eip" "nat_eip" {
+    domain = "vpc"
+
+    tags = {
+      Name = "nat-eip"
+    }
+}
+# NAT Gateway
+resource "aws_nat_gateway" "nat_gw" {
+    allocation_id = aws_eip.nat_eip.id
+    subnet_id = aws_subnet.public_subnet[count.index].id
+
+    tags = {
+        Name = "nat-gateway"
+    }
+    depends_on = [aws_internet_gateway.igw]
+}
---
+++ terraform/deployment/modules/vpc/variables.tf
@@ -0,0 +1,16 @@
+variable "cidr_block" {
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnets_cidrs" {
+    type = list(string)
+    description = "CIDR blocks for my 2 public subnets"
+    default = [ "10.0.1.0/24", "10.0.2.0/24" ]
+}
+
+variable "private_subnets_cidrs" {
+    type = list(string)
+    description = "CIDR blocks for my 2 private subnets"
+    default = [ "10.0.3.0/24", "10.0.4.0/24" ]
+}
