13ee51a78f8db60888a7607750151b6ac80e4de7
shamimchadgit <shamimchaudhury133@gmail.com>
2026-03-25 16:08:29 +0000

    Add - variables for subnets

--- terraform/deployment/modules/eks/variables.tf
+++ terraform/deployment/modules/eks/variables.tf
@@ -1,20 +1,20 @@
 variable "vpc_id" {
-    type = string
-    description = "ID for my VPC"
+  type        = string
+  description = "ID for my VPC"
 }
 
 variable "cluster_name" {
-    type = string
-    description = "EKS cluster name"
-    default = "eks-keto-cluster"
+  type        = string
+  description = "EKS cluster name"
+  default     = "eks-keto-cluster"
 }
 
 variable "private_subnet_ids" {
-    type = list(string)
-    description = "Private subnet worker nodes will run in"
+  type        = list(string)
+  description = "Private subnet worker nodes will run in"
 }
 
 variable "public_subnet_ids" {
-    type = list(string)
-    description = "Public subnet control plane will run in"
+  type        = list(string)
+  description = "Public subnet control plane will run in"
 }
