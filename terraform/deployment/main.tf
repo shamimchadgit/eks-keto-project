e90ae4bc3c7641d044409e5075c4b92133e61616
shamimchadgit <shamimchaudhury133@gmail.com>
2026-03-25 15:54:40 +0000

    Add - public and private subnet ids to eks module

--- terraform/deployment/main.tf
+++ terraform/deployment/main.tf
@@ -5,5 +5,6 @@ module "vpc" {
 module "eks" {
     source = "./modules/eks"
     vpc_id = module.vpc.vpc_id
-    private_subnet_ids = [module.vpc.private_subnet_ids]
+    private_subnet_ids = module.vpc.private_subnet_ids
+    public_subnet_ids = module.vpc.public_subnet_ids
 }
