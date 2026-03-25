13210fa8fe56280cccc3899b8d9896c60791edaa
shamimchadgit <shamimchaudhury133@gmail.com>
2026-03-25 14:35:37 +0000

    Add - eks module to root

--- terraform/deployment/main.tf
+++ terraform/deployment/main.tf
@@ -1,3 +1,9 @@
 module "vpc" {
   source = "./modules/vpc"
 }
+
+module "eks" {
+    source = "./modules/eks"
+    vpc_id = module.vpc.vpc_id
+    private_subnet_ids = [module.vpc.private_subnet_ids]
+}
