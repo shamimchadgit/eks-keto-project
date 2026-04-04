d8186e5f14f4785203588c94876b6766f1f04c19
shamimchadgit <shamimchaudhury133@gmail.com>
2026-04-04 20:49:42 +0100

    Variable - added cluster oidc issuer url variable so can hook to main.tf the output from eks module

--- terraform/deployment/modules/external-dns/variables.tf
+++ terraform/deployment/modules/external-dns/variables.tf
@@ -1,9 +1,14 @@
 variable "hosted_zone_id" {
-    type = string
-    description = "My hosted zone id"
+  type        = string
+  description = "My hosted zone id"
 }
 
 variable "eks_oidc_provider_arn" {
+  type        = string
+  description = "The ARN of my OIDC provider K8 module"
+}
+
+variable "cluster_oidc_issuer_url" {
     type = string
-    description = "The ARN of my OIDC provider K8 module"
+    description = "Kubernetes managed nodes module cluster oidc issuer url"
 }
