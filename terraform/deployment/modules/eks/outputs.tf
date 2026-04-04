f4dfd69076cf1f7bf9f5642c21664aab6ccde2dc
shamimchadgit <shamimchaudhury133@gmail.com>
2026-04-04 20:47:17 +0100

    Output - cluster oidc issuer url for external-dns iam role

--- terraform/deployment/modules/eks/outputs.tf
+++ terraform/deployment/modules/eks/outputs.tf
@@ -1,3 +1,7 @@
 output "oidc_provider_arn" {
-    value = module.eks.oidc_provider_arn
+  value = module.eks.oidc_provider_arn
+}
+
+output "cluster_oidc_issuer_url" {
+    value = module.eks.cluster_oidc_issuer_url
 }
