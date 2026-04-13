62c77397d71c886abd21839efcb4c5edb908e6b8
shamimchadgit <shamimchaudhury133@gmail.com>
2026-04-13 13:47:19 +0100

    Add - kubernetes and helm providers

--- terraform/deployment/providers.tf
+++ terraform/deployment/providers.tf
@@ -1,3 +1,5 @@
+# Terraform 
+
 terraform {
   required_providers {
     aws = {
@@ -9,4 +11,69 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
+}
+
+# Kubernetes
+
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "3.0.1"
+    }
+  }
+}
+
+provider "kubernetes" {
+  host = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
+  
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    command = "aws"
+  }
+}
+
+
+# Helm
+
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.1.1"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes = {
+    host = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      command = "aws"
+    }
+
+
+
+
+
+    config_path = "~/.kube/config"
+  }
+
+  registries = [
+    {
+      url      = "oci://localhost:5000"
+      username = "username"
+      password = "password"
+    },
+    {
+      url      = "oci://private.registry"
+      username = "username"
+      password = "password"
+    }
+  ]
 }
