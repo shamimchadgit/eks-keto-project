5ce413c4e2cbb8dccf8baa6bf54f386206870dc9
shamimchadgit <shamimchaudhury133@gmail.com>
2026-04-13 13:46:51 +0100

    Add - cluster name, certificate, endpoint, oidc issuer url and arn

--- terraform/deployment/modules/eks/outputs.tf
+++ terraform/deployment/modules/eks/outputs.tf
@@ -5,3 +5,16 @@ output "oidc_provider_arn" {
 output "cluster_oidc_issuer_url" {
     value = module.eks.cluster_oidc_issuer_url
 }
+
+output "cluster_endpoint" {
+    value = module.eks.cluster_endpoint
+}
+
+output "cluster_ca_certificate" {
+    value = module.eks.cluster_certificate_authority_data
+}
+
+output "cluster_name" {
+    value = module.eks.cluster_name
+  
+}
