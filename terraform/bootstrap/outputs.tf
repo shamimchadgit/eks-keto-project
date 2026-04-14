622c3f0a9752aea50072caf385d922554c7ba70e
shamimchadgit <shamimchaudhury133@gmail.com>
2026-04-14 10:54:58 +0100

    Changed - output name fro hosted zone to reflect arn instead of zone id

--- terraform/bootstrap/outputs.tf
+++ terraform/bootstrap/outputs.tf
@@ -2,6 +2,6 @@ output "name_servers" {
     value = aws_route53_zone.eks_zone.name_servers
 }
 
-output "hosted_zone_id" {
-    value = aws_route53_zone.eks_zone.zone_id
+output "hosted_zone_arn" {
+    value = aws_route53_zone.eks_zone.arn
 }
