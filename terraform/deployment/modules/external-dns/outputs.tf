8f84c96e7039f3cc4990fff0ba2b408a74d9ce40
shamimchadgit <shamimchaudhury133@gmail.com>
2026-04-27 11:40:37 +0100

    Add - comment for clarity on the external dns role arn

--- terraform/deployment/modules/external-dns/outputs.tf
+++ terraform/deployment/modules/external-dns/outputs.tf
@@ -1,3 +1,6 @@
 output "external_dns_role_arn" {
   value = aws_iam_role.external_dns_role.arn
+  description = "Output arn for external dns role so K8 service a/c can assume role"
 }
+
+# gives permissions directly to POD (IRSA)
